2cbee6a7e15c0e79e5d40b96f84dd37a721e31c9
iloElias <murilo7456@gmail.com>
2025-03-23 19:30:52 -0300

    chore: updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,34 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// Public paths for user initial access, only accessible if the user has no token
 export const PUBLIC_PATHS = [
   "/login",
   "/sign-up",
   "/recover-token",
   "/reset-password",
 ];
+// Paths that require at least a browser agent and a token
 export const PUBLIC_AUTH_PATHS = ["/auth-code", "/auth-with"];
+// Paths that require authentication
+// (browser agent, token, and authenticated cookie)
 export const USER_PATHS = ["/", "/dashboard", "/user", "/profile", "/settings"];
 
 export const AUTH_TOKEN_KEY = `${process.env.NEXT_PUBLIC_SERVICE_ID}_auth_token`;
-export const AUTHENTICATED_KEY = `${process.env.NEXT_PUBLIC_SERVICE_ID}_authenticated`;
+export const AUTHENTICATED_KEY = `${process.env.NEXT_PUBLIC_SERVICE_ID}_authenticated`; // If this cookie is set, the user is authenticated
 export const AUTH_BROWSER_AGENT_KEY = `${process.env.NEXT_PUBLIC_SERVICE_ID}_auth_browser_agent`;
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
   const hasBrowserAgent = request.cookies.has(AUTH_BROWSER_AGENT_KEY);
   const hasToken = request.cookies.has(AUTH_TOKEN_KEY);
-
-  const hasToAuthenticate = request.cookies.has(AUTHENTICATED_KEY);
-
-  const pathname = request.nextUrl.pathname;
+  const isAuthenticated = request.cookies.has(AUTHENTICATED_KEY);
 
   if ((!hasBrowserAgent || !hasToken) && !PUBLIC_PATHS.includes(pathname)) {
-    const url = new URL(PUBLIC_PATHS[0], request.url);
-    return NextResponse.redirect(url.toString());
+    const redirectUrl = new URL(PUBLIC_PATHS[0], request.url);
+    return NextResponse.redirect(redirectUrl.toString());
   }
 
-  if (hasBrowserAgent && hasToken && hasToAuthenticate && !PUBLIC_AUTH_PATHS.includes(pathname)) {
-    const url = new URL(PUBLIC_AUTH_PATHS[0], request.url);
-    return NextResponse.redirect(url.toString());
+  if (hasBrowserAgent && hasToken && !isAuthenticated && !PUBLIC_AUTH_PATHS.includes(pathname)) {
+    const redirectUrl = new URL(PUBLIC_AUTH_PATHS[0], request.url);
+    return NextResponse.redirect(redirectUrl.toString());
   }
 
   return NextResponse.next();
