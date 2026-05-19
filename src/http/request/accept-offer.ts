9b81c3b1a3a037058e597eaa894fd6ff60cac365
Murilo Elias <106710958+iloElias@users.noreply.github.com>
2026-05-18 23:18:19 -0300

    Change acceptOffer to use PUT instead of POST

--- src/http/request/accept-offer.ts
+++ src/http/request/accept-offer.ts
@@ -1,5 +1,5 @@
 import { api } from "@/service/api";
 
 export const acceptOffer = (offerId: string) => {
-  return api.post(`/offer/${offerId}/accept`);
+  return api.put(`/offer/${offerId}/accept`);
 };
